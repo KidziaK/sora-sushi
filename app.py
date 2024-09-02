84d98a40b38a891b98f944c2849f00793cf25fd7
mikolaj <mikolaj.kida@skyengine.ai>
2024-09-02 16:43:11 +0200

    feat: added working counter to js.

--- app.py
+++ app.py
@@ -13,32 +13,41 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
-# TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-# TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
-# TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
-# TO_PHONE_NUMBER = os.getenv('TO_PHONE_NUMBER')
+TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
+TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
+TO_PHONE_NUMBER = os.getenv('TO_PHONE_NUMBER')
 
 
-# client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
 
 sushi_count = 0
 
-# def send_sms():
-#     global sushi_count
-#     client.messages.create(
-#         to=TO_PHONE_NUMBER,
-#         from_=TWILIO_PHONE_NUMBER,
-#         body=f"Dzień dobry, ze Sky Engine jest {sushi_count} chętnych na sushi.\nPozdrawiam."
-#     )
-#     sushi_count = 0
+def send_sms():
+    global sushi_count
+    client.messages.create(
+        to=TO_PHONE_NUMBER,
+        from_=TWILIO_PHONE_NUMBER,
+        body=f"Dzień dobry, z tej strony Sky Engine. Na sushi od nas jest: {sushi_count} chętnych.\nPozdrawiam."
+    )
 
-# scheduler = BackgroundScheduler()
-# scheduler.add_job(send_sms, 'cron', day_of_week='tue,thu', hour=10, minute=0)
-# scheduler.start()
+def reset_shushi_count():
+    global sushi_count
+    sushi_count = 0
 
-# @app.on_event("shutdown")
-# def shutdown_event():
-#     scheduler.shutdown()
+scheduler = BackgroundScheduler()
+scheduler.add_job(send_sms, 'cron', day_of_week='tue,thu', hour=10, minute=0)
+scheduler.add_job(reset_shushi_count, 'cron', day_of_week='mon,wed', hour=23, minute=59)
+scheduler.start()
+
+@app.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
+
+@app.post('/')
+async def incerement_sushi_count(request: Request):
+    global sushi_count
+    sushi_count += 1
 
 @app.get('/')
 async def hello_world(request: Request):
